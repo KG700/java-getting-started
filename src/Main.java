38d825a19a8842753f30b6728a7d9f920dfb6844
Karla Gardiner <karla.gardiner@gmail.com>
2020-09-17 12:56:56 +0100

    Added error handling to mortgage calculator

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,27 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal: ");
+        System.out.print("Principal (£1K - £1M): ");
         int principal = scanner.nextInt();
+        while (principal < 1_000 || principal > 1_000_000) {
+            System.out.print("Enter a number between 1,000 and 1,000,000: ");
+            principal = scanner.nextInt();
+        }
 
         System.out.print("Annual Interest Rate: ");
         float annualInterest = scanner.nextFloat();
+        while (annualInterest <= 0 || annualInterest > 30) {
+            System.out.print("Enter a value greater than 0 and less than or equal to 30: ");
+            annualInterest = scanner.nextFloat();
+        }
         float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
 
         System.out.print("Period (Years): ");
         byte years = scanner.nextByte();
+        while (years < 1 || years > 30) {
+            System.out.print("Enter a value between 1 and 30: ");
+            years = scanner.nextByte();
+        }
         int numberOfPayments = years * MONTHS_IN_YEAR;
 
         double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
